efb32392eabd8e88f0b5348f91196c2eef0b22ce
MrHohn <zzhonzi@gmail.com>
2015-06-24 00:01:47 -0400

    added 10 Regular Expression Matching, recursive version

---
+++ 10-Regular-Expression-Matching/RegularMatch.java
@@ -0,0 +1,70 @@
+// Regular Expression Matching Total Accepted: 44906 Total Submissions: 217185 My Submissions Question Solution 
+// Implement regular expression matching with support for '.' and '*'.
+
+// '.' Matches any single character.
+// '*' Matches zero or more of the preceding element.
+
+// The matching should cover the entire input string (not partial).
+
+// The function prototype should be:
+// bool isMatch(const char *s, const char *p)
+
+// Some examples:
+// isMatch("aa","a") -> false
+// isMatch("aa","aa") -> true
+// isMatch("aaa","aa") -> false
+// isMatch("aa", "a*") -> true
+// isMatch("aa", ".*") -> true
+// isMatch("ab", ".*") -> true
+// isMatch("aab", "c*a*b") -> true
+// Hide Tags Dynamic Programming Backtracking String
+
+public class RegularMatch {
+    public boolean isMatch(String s, String p) {
+		if (p.length() == 0) {
+			return s.length() == 0;
+		}
+
+		if (p.length() == 1) {
+			if (s.length() != 1) {
+				return false;
+			}
+			if (p.charAt(0) == '*') {
+				return false;
+			}
+
+			if (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.') {
+				return true;
+			}
+
+			return false;
+		}
+
+		if (p.charAt(1) != '*') {
+			if (s.length() == 0) {
+				return false;
+			}
+
+			if (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.') {
+				return isMatch(s.substring(1), p.substring(1));
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			if (isMatch(s, p.substring(2))) {
+				return true;
+			}
+
+			int i = 0;
+			while (i < s.length() && (s.charAt(i) == p.charAt(0) || p.charAt(0) == '.')) {
+				if (isMatch(s.substring(i + 1), p.substring(2))) {
+					return true;
+				}
+				++i;
+			}
+			return false;
+		}
+    }
+}
