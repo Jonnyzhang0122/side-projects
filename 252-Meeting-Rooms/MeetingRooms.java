5258ac19b78f6f7fa5ff36ca692529fa3d300463
MrHohn <zzhonzi@gmail.com>
2015-09-12 00:12:38 -0400

    Revise 252 Meeting Rooms, 100% version

--- 252-Meeting-Rooms/MeetingRooms.java
+++ 252-Meeting-Rooms/MeetingRooms.java
@@ -19,38 +19,58 @@
  * }
  */
 public class Solution {
+    // public boolean canAttendMeetings(Interval[] intervals) {
+    //     if (intervals == null || intervals.length == 0) {
+    //         return true;
+    //     }
+        
+    //     int[] starts = new int[intervals.length];
+    //     int[] ends = new int[intervals.length];
+        
+    //     for (int i = 0; i < intervals.length; ++i) {
+    //         starts[i] = intervals[i].start;
+    //         ends[i] = intervals[i].end;
+    //     }
+        
+    //     Arrays.sort(starts);
+    //     Arrays.sort(ends);
+        
+    //     int startPos = 0, endPos = 0, count = 0;
+        
+    //     while (startPos < intervals.length) {
+    //         if (starts[startPos] < ends[endPos]) {
+    //             ++count;
+    //             ++startPos;
+    //         }
+    //         else {
+    //             --count;
+    //             ++endPos;
+    //         }
+    //         if (count > 1) {
+    //             return false;
+    //         }
+    //     }
+        
+    //     return true;
+    // }
+
     public boolean canAttendMeetings(Interval[] intervals) {
         if (intervals == null || intervals.length == 0) {
             return true;
         }
-        
-        int[] starts = new int[intervals.length];
-        int[] ends = new int[intervals.length];
-        
-        for (int i = 0; i < intervals.length; ++i) {
-            starts[i] = intervals[i].start;
-            ends[i] = intervals[i].end;
-        }
-        
-        Arrays.sort(starts);
-        Arrays.sort(ends);
-        
-        int startPos = 0, endPos = 0, count = 0;
-        
-        while (startPos < intervals.length) {
-            if (starts[startPos] < ends[endPos]) {
-                ++count;
-                ++startPos;
-            }
-            else {
-                --count;
-                ++endPos;
-            }
-            if (count > 1) {
-                return false;
-            }
-        }
-        
-        return true;
-    }
+
+    	Arrays.sort(intervals, new Comparator<Interval>() {
+    		public int compare(Interval interval1, Interval interval2){
+    			return interval1.start - interval2.start;
+    		}
+    	});
+
+    	for (int i = 1; i < intervals.length; ++i) {
+    		if (intervals[i].start < intervals[i - 1].end) {
+    			return false;
+    		}
+    	}
+
+    	return true;
+	}
 }
