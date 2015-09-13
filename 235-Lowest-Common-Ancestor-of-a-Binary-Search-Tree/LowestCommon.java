0ed0b01c777b9f642fedea4e856815ea5824c697
MrHohn <zzhonzi@gmail.com>
2015-09-12 22:01:21 -0400

    Added 235 Lowest Common Ancestor of BST, iterative version

--- 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/LowestCommon.java
+++ 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/LowestCommon.java
@@ -50,8 +50,18 @@ public class Solution {
         }
         
         while (root != null) {
-        	
+            if (root.val > p.val && root.val > q.val) {
+                root = root.left;
+            }
+            else if (root.val < p.val && root.val < q.val) {
+                root = root.right;
+            }
+            else {
+                return root;
+            }
         }
+
+        return root;
     }
 
 }
