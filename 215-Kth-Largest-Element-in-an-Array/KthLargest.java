43cd82ffbe664726b1a8f33e575ce4748f37c374
MrHohn <zzhonzi@gmail.com>
2015-09-14 17:23:36 -0400

    Added few more solutions for 215

--- 215-Kth-Largest-Element-in-an-Array/KthLargest.java
+++ 215-Kth-Largest-Element-in-an-Array/KthLargest.java
@@ -1,5 +1,6 @@
 
 public class Solution {
+    // max heap version
     public int findKthLargest(int[] nums, int k) {
         int len = nums.length;
         PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(len, IntComparator);
@@ -22,4 +23,86 @@ public class Solution {
             return right - left;
         }
     };
+    
+    // sort version
+    /*
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        
+        return nums[nums.length - k];
+    }
+    */
+    
+    /*
+    // min heap version
+    public int findKthLargest(int[] nums, int k) {
+        int len = nums.length;
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>();
+        
+        heap.add(nums[0]);
+        for (int i = 1; i < len; ++i) {
+            if (heap.size() < k || nums[i] > heap.peek()) {
+                heap.add(nums[i]);                
+            }
+            
+            // make sure there are only the K largest element stored in the heap
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+        
+        return heap.peek();
+    }
+    */
+
+    /*
+    // partition version
+    public int findKthLargest(int[] nums, int k) {
+        int len = nums.length;
+        return quickSelect(nums, 0, len - 1, k);
+    }
+    
+    // return the Kth largest number
+    public int quickSelect(int[] nums, int begin, int end, int k) {
+        // shuffle the pivot to avoid worst case
+        //int pivotIndex = randInt(begin, end);
+        // put the pivot to the end
+        //swap(nums, pivotIndex, end);
+        int pivot = nums[end];
+        int pos = begin - 1;
+        for (int i = begin; i < end; ++i) {
+            if (nums[i] > pivot) {
+                ++pos;
+                // System.out.println("num is bigger, swap" + i + pos);
+                swap(nums, i, pos);
+            }
+        }
+        System.out.println("here");
+        swap(nums, pos + 1, end);
+        
+        if (pos + 1 == k) {
+            return nums[k];
+        }
+        else if (pos + 1 > k) {
+            return quickSelect(nums, begin, pos, k);
+        }
+        else {
+            return quickSelect(nums, pos + 1, end, k - (pos + 1 - begin));
+        }
+    }
+    
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    */
+    
+    /*
+    public int randInt(int min, int max) {
+        Random rand = new Random();
+        int result = rand.nextInt(max - min + 1) + min;
+        return result;
+    }
+    */
 }
