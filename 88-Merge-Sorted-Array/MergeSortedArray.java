c7ff924ca51fbf1fd8e91344c6b0f21691267384
MrHohn <zzhonzi@gmail.com>
2015-10-25 17:08:47 -0400

    Rewrite 88 Merge Sorted Array, inplace version

--- 88-Merge-Sorted-Array/MergeSortedArray.java
+++ 88-Merge-Sorted-Array/MergeSortedArray.java
@@ -6,73 +6,98 @@
 
 // Hide Tags Array Two Pointers
 
-public class MergeSortedArray {
-    public void merge(int A[], int m, int B[], int n) {
-        int execute = 0;
-        if (m == 0 && n == 0) {
-        	execute = 1;
-        }
-        else if (m == 0) {
-        	for (int i = 0; i < n; ++i) {
-        		A[i] = B[i];
-        	}
-        	execute = 1;
-        }
-        else if (n == 0) {
-        	execute = 1;
-        }
+// public class MergeSortedArray {
+//     public void merge(int A[], int m, int B[], int n) {
+//         int execute = 0;
+//         if (m == 0 && n == 0) {
+//         	execute = 1;
+//         }
+//         else if (m == 0) {
+//         	for (int i = 0; i < n; ++i) {
+//         		A[i] = B[i];
+//         	}
+//         	execute = 1;
+//         }
+//         else if (n == 0) {
+//         	execute = 1;
+//         }
 
-        if (execute == 0) {
-        	int[] C = new int[m + n];
-        	int APoint = 0;
-        	int BPoint = 0;
-        	int CPoint = 0;
-        	while (true) {
-        		if (A[APoint] <= B[BPoint]) {
-        			C[CPoint] = A[APoint];
-        			++APoint;
-        		}
-        		else {
-        			C[CPoint] = B[BPoint];
-        			++BPoint;
-        		}
-    			++CPoint;
+//         if (execute == 0) {
+//         	int[] C = new int[m + n];
+//         	int APoint = 0;
+//         	int BPoint = 0;
+//         	int CPoint = 0;
+//         	while (true) {
+//         		if (A[APoint] <= B[BPoint]) {
+//         			C[CPoint] = A[APoint];
+//         			++APoint;
+//         		}
+//         		else {
+//         			C[CPoint] = B[BPoint];
+//         			++BPoint;
+//         		}
+//     			++CPoint;
 
-    			if (CPoint == m + n) {
-    				break;
-    			}
-    			if (APoint == m) {
-    				while (CPoint < m + n) {
-    					C[CPoint] = B[BPoint];
-    					++CPoint;
-    					++BPoint;
-    				}
-    				break;
-    			}
-    			if (BPoint == n) {
-    				while (CPoint < m + n) {
-    					C[CPoint] = A[APoint];
-    					++CPoint;
-    					++APoint;
-    				}
-    				break;
-    			}
+//     			if (CPoint == m + n) {
+//     				break;
+//     			}
+//     			if (APoint == m) {
+//     				while (CPoint < m + n) {
+//     					C[CPoint] = B[BPoint];
+//     					++CPoint;
+//     					++BPoint;
+//     				}
+//     				break;
+//     			}
+//     			if (BPoint == n) {
+//     				while (CPoint < m + n) {
+//     					C[CPoint] = A[APoint];
+//     					++CPoint;
+//     					++APoint;
+//     				}
+//     				break;
+//     			}
     			
-        	}
+//         	}
 
-        	for (int i = 0; i < m + n; ++i) {
-        		A[i] = C[i];
-        	}
+//         	for (int i = 0; i < m + n; ++i) {
+//         		A[i] = C[i];
+//         	}
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//     	MergeSortedArray Solution = new MergeSortedArray();
+//     	int[] A = {1, 3, 5, 7, 0, 0, 0, 0};
+//     	int[] B = {2, 10,11, 18};
+//     	Solution.merge(A, 4, B, B.length);
+//     	for (int i : A) {
+//     		System.out.println(i);
+//     	}
+//     }
+// }
+
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int cur = m + n - 1;
+        
+        while (p2 >= 0 && p1 >= 0) {
+            if (nums1[p1] >= nums2[p2]) {
+                nums1[cur] = nums1[p1];
+                --p1;
+            }
+            else {
+                nums1[cur] = nums2[p2];
+                --p2;
+            }
+            --cur;
         }
-    }
-
-    public static void main(String[] args) {
-    	MergeSortedArray Solution = new MergeSortedArray();
-    	int[] A = {1, 3, 5, 7, 0, 0, 0, 0};
-    	int[] B = {2, 10,11, 18};
-    	Solution.merge(A, 4, B, B.length);
-    	for (int i : A) {
-    		System.out.println(i);
-    	}
+        while (p2 >= 0) {
+            nums1[cur] = nums2[p2];
+            --cur;
+            --p2;
+        }
     }
 }
