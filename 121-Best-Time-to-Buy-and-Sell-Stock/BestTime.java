cb563bf081389ede937b1bbf8e384ec412095272
MrHohn <zzhonzi@gmail.com>
2015-10-26 15:18:09 -0400

    Rewrote 121 Best Time Stock

--- 121-Best-Time-to-Buy-and-Sell-Stock/BestTime.java
+++ 121-Best-Time-to-Buy-and-Sell-Stock/BestTime.java
@@ -6,22 +6,39 @@
 // Hide Tags Array Dynamic Programming
 
 public class BestTime {
+    // public int maxProfit(int[] prices) {
+    //     if (prices == null || prices.length == 0) {
+    //     	return 0;
+    //     }
+
+    //     int min = Integer.MAX_VALUE;
+    //     int profit = 0;
+    //     for (int i = 0; i < prices.length; ++i) {
+    //     	if (prices[i] < min) {
+    //     		min = prices[i];
+    //     	}
+    //     	if (prices[i] - min > profit) {
+    //     		profit = prices[i] - min;
+    //     	}
+    //     }
+
+    //     return profit;
+    // }
+
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0) {
-        	return 0;
+        int len = prices.length;
+        if (len == 0) {
+            return 0;
         }
-
-        int min = Integer.MAX_VALUE;
-        int profit = 0;
-        for (int i = 0; i < prices.length; ++i) {
-        	if (prices[i] < min) {
-        		min = prices[i];
-        	}
-        	if (prices[i] - min > profit) {
-        		profit = prices[i] - min;
-        	}
+        
+        int min = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < len; ++i) {
+            min = Math.min(min, prices[i]);
+            int curProfit = prices[i] - min;
+            maxProfit = Math.max(maxProfit, curProfit);
         }
-
-        return profit;
+        
+        return maxProfit;
     }
 }
