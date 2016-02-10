c6da34b0991fd16d8b32073d0c275bfdb63d57ea
MrHohn <zzhonzi@gmail.com>
2016-02-10 16:44:22 -0500

    Added substring without repeat, hashmap version

--- 3-Longest-Substring-Without-Repeating-Characters/Longest.java
+++ 3-Longest-Substring-Without-Repeating-Characters/Longest.java
@@ -1,31 +1,50 @@
 import java.util.*;
 
 public class Longest {
-    // rewrote version
+    // hashmap version
     public int lengthOfLongestSubstring(String s) {
         if (s.length() <= 1) {
             return s.length();
         }
         
-        HashSet<Character> set = new HashSet<>();
-        set.add(s.charAt(0));
+        HashMap<Character, Integer> map = new HashMap<>();
         int max = 1, start = 0;
+        map.put(s.charAt(0), 0);
         for (int i = 1; i < s.length(); ++i) {
-            char cur = s.charAt(i);
-            if (!set.contains(cur)) {
-                set.add(cur);
+            if (map.containsKey(s.charAt(i))) {
+                start = Math.max(start, map.get(s.charAt(i)) + 1);
             }
-            else {
-                while (s.charAt(start) != cur) {
-                    set.remove(s.charAt(start));
-                    ++start;
-                }
-                ++start;
-            }
+            map.put(s.charAt(i), i);
             max = Math.max(max, i - start + 1);
         }
         return max;
     }
+
+    // rewrote hashset version
+    // public int lengthOfLongestSubstring(String s) {
+    //     if (s.length() <= 1) {
+    //         return s.length();
+    //     }
+        
+    //     HashSet<Character> set = new HashSet<>();
+    //     set.add(s.charAt(0));
+    //     int max = 1, start = 0;
+    //     for (int i = 1; i < s.length(); ++i) {
+    //         char cur = s.charAt(i);
+    //         if (!set.contains(cur)) {
+    //             set.add(cur);
+    //         }
+    //         else {
+    //             while (s.charAt(start) != cur) {
+    //                 set.remove(s.charAt(start));
+    //                 ++start;
+    //             }
+    //             ++start;
+    //         }
+    //         max = Math.max(max, i - start + 1);
+    //     }
+    //     return max;
+    // }
 
     // public int lengthOfLongestSubstring(String s) {
     //     if (s == null || s.length() == 0 || s.equals("")) {
