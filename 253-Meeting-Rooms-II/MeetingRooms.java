9f38e8715264394175daaaf044f1c0e6acee2e25
MrHohn <zzhonzi@gmail.com>
2016-02-10 14:45:39 -0500

    Rewrote meeting room II

--- 253-Meeting-Rooms-II/MeetingRooms.java
+++ 253-Meeting-Rooms-II/MeetingRooms.java
@@ -1,35 +1,70 @@
-
 public class Solution {
     public int minMeetingRooms(Interval[] intervals) {
-        if (intervals == null || intervals.length == 0) {
+        if (intervals.length == 0) {
             return 0;
         }
-        int[] begin = new int[intervals.length];
-        int[] end = new int[intervals.length];
+        
+        int[] starts = new int[intervals.length];
+        int[] ends   = new int[intervals.length];
         
         for (int i = 0; i < intervals.length; ++i) {
-            begin[i] = intervals[i].start;
-            end[i] = intervals[i].end;
+            starts[i] = intervals[i].start;
+            ends[i]   = intervals[i].end;
         }
         
-        Arrays.sort(begin);
-        Arrays.sort(end);
+        Arrays.sort(starts);
+        Arrays.sort(ends);
         
-        int beginPos = 0, endPos = 0, max = 0, count = 0;
-        
-        while (beginPos < intervals.length && endPos < intervals.length) {
-            if (begin[beginPos] < end[endPos]) {
+        int sPos = 0, ePos = 0;
+        int max = 0, count = 0;
+        while (sPos < intervals.length && ePos < intervals.length) {
+            if (starts[sPos] < ends[ePos]) {
                 ++count;
-                ++beginPos;
+                ++sPos;
+                max = Math.max(max, count);
             }
             else {
                 --count;
-                ++endPos;
+                ++ePos;
             }
-            if (count > max)
-                max = count;
         }
         
         return max;
     }
 }
+
+
+// public class Solution {
+//     public int minMeetingRooms(Interval[] intervals) {
+//         if (intervals == null || intervals.length == 0) {
+//             return 0;
+//         }
+//         int[] begin = new int[intervals.length];
+//         int[] end = new int[intervals.length];
+        
+//         for (int i = 0; i < intervals.length; ++i) {
+//             begin[i] = intervals[i].start;
+//             end[i] = intervals[i].end;
+//         }
+        
+//         Arrays.sort(begin);
+//         Arrays.sort(end);
+        
+//         int beginPos = 0, endPos = 0, max = 0, count = 0;
+        
+//         while (beginPos < intervals.length && endPos < intervals.length) {
+//             if (begin[beginPos] < end[endPos]) {
+//                 ++count;
+//                 ++beginPos;
+//             }
+//             else {
+//                 --count;
+//                 ++endPos;
+//             }
+//             if (count > max)
+//                 max = count;
+//         }
+        
+//         return max;
+//     }
+// }
