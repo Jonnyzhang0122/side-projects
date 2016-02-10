7493c45b485f462e19ca077b0e11819bd5b8472b
MrHohn <zzhonzi@gmail.com>
2016-02-10 16:26:36 -0500

    Rewrote Substring without repeat

--- 3-Longest-Substring-Without-Repeating-Characters/Longest.java
+++ 3-Longest-Substring-Without-Repeating-Characters/Longest.java
@@ -1,42 +1,63 @@
-// Longest Substring Without Repeating Characters Total Accepted: 72789 Total Submissions: 353671 My Submissions Question Solution 
-// Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
-
-// Hide Tags Hash Table Two Pointers String
-
 import java.util.*;
 
 public class Longest {
+    // rewrote version
     public int lengthOfLongestSubstring(String s) {
-        if (s == null || s.length() == 0 || s.equals("")) {
-        	return 0;
+        if (s.length() <= 1) {
+            return s.length();
         }
-
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        int max = 1;
-        int i = 0;
-        int j = 0;
-    	while (j < s.length()) {
-        	if (!map.containsKey(s.charAt(j))) {
-        		map.put(s.charAt(j), j);
-        		max = Math.max(max, j - i + 1);
-        		++j;
-        	}
-        	else {
-        		while (map.containsKey(s.charAt(j))) {
-        			map.remove(s.charAt(i));
-        			++i;
-        		}
-        	}
-    	}
-
+        
+        HashSet<Character> set = new HashSet<>();
+        set.add(s.charAt(0));
+        int max = 1, start = 0;
+        for (int i = 1; i < s.length(); ++i) {
+            char cur = s.charAt(i);
+            if (!set.contains(cur)) {
+                set.add(cur);
+            }
+            else {
+                while (s.charAt(start) != cur) {
+                    set.remove(s.charAt(start));
+                    ++start;
+                }
+                ++start;
+            }
+            max = Math.max(max, i - start + 1);
+        }
         return max;
     }
 
-    public static void main(String[] args) {
-    	Longest A = new Longest();
-    	String s = "cdd";
-    	System.out.println(A.lengthOfLongestSubstring(s));
-    }
+    // public int lengthOfLongestSubstring(String s) {
+    //     if (s == null || s.length() == 0 || s.equals("")) {
+    //     	return 0;
+    //     }
+
+    //     HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+    //     int max = 1;
+    //     int i = 0;
+    //     int j = 0;
+    // 	while (j < s.length()) {
+    //     	if (!map.containsKey(s.charAt(j))) {
+    //     		map.put(s.charAt(j), j);
+    //     		max = Math.max(max, j - i + 1);
+    //     		++j;
+    //     	}
+    //     	else {
+    //     		while (map.containsKey(s.charAt(j))) {
+    //     			map.remove(s.charAt(i));
+    //     			++i;
+    //     		}
+    //     	}
+    // 	}
+
+    //     return max;
+    // }
+
+    // public static void main(String[] args) {
+    // 	Longest A = new Longest();
+    // 	String s = "cdd";
+    // 	System.out.println(A.lengthOfLongestSubstring(s));
+    // }
 }
 
 // public class Solution {
