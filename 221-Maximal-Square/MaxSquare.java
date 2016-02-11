16bcead77c533bce2ca64e774f05a30247cbb706
MrHohn <zzhonzi@gmail.com>
2016-02-11 00:22:37 -0500

    Rewrote Maximal Square, combined loops

--- 221-Maximal-Square/MaxSquare.java
+++ 221-Maximal-Square/MaxSquare.java
@@ -1,19 +1,27 @@
-// Maximal Square Total Accepted: 4471 Total Submissions: 22060 My Submissions Question Solution 
-// Given a 2D binary matrix filled with 0's and 1's, find the largest square containing all 1's and return its area.
+// optimized version, combined loops
+public class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
 
-// For example, given the following matrix:
+        int[][] dp = new int[matrix.length + 1][matrix[0].length + 1];
+        int maxLength = 0;        
+        for (int i = 1; i <= matrix.length; ++i) {
+            for (int j = 1; j <= matrix[0].length; ++j) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    int temp = Math.min(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = Math.min(temp, dp[i - 1][j - 1]) + 1;
+                    maxLength = Math.max(maxLength, dp[i][j]);
+                }
+            }
+        }
+        
+        return maxLength * maxLength;
+    }
+}
 
-// 1 0 1 0 0
-// 1 0 1 1 1
-// 1 1 1 1 1
-// 1 0 0 1 0
-// Return 4.
-// Credits:
-// Special thanks to @Freezen for adding this problem and creating all test cases.
-
-// Hide Tags Dynamic Programming
-
-public class MaxSquare {
+/*public class MaxSquare {
     public int maximalSquare(char[][] matrix) {
 		if (matrix.length == 0 || matrix[0].length == 0) {
 			return 0;
@@ -51,3 +59,4 @@ public class MaxSquare {
 		return maxArea * maxArea;
     }
 }
+*/
