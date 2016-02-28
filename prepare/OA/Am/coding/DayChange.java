ec8c42da591cfdf4650c1236d09677628244b2f6
MrHohn <zzhonzi@gmail.com>
2016-02-28 15:33:13 -0500

    Rewrote Day Change in place

--- prepare/OA/Am/coding/DayChange.java
+++ prepare/OA/Am/coding/DayChange.java
@@ -11,25 +11,28 @@ public class DayChange {
 		}
 
 		for (int i = 0; i < days; ++i) {
-			int[] temp = new int[cells.length];
-			temp[0] = cells[1];
+			int prev = cells[0];
+			// the first cell depends on next one
+			cells[0] = cells[1];
 			for (int j = 1; j < cells.length - 1; ++j) {
-				if (cells[j - 1] != cells[j + 1]) {
-					temp[j] = 1;
+				int temp = cells[j];
+				if (prev != cells[j + 1]) {
+					cells[j] = 1;
 				}
 				else {
-					temp[j] = 0;
+					cells[j] = 0;
 				}
+				prev = temp;
 			}
-			temp[cells.length - 1] = cells[cells.length - 2];
-			cells = temp;
+			// the last cell depends on the second last cell
+			cells[cells.length - 1] = prev;
 		}
 		return cells;
 	}
 
 	public static void main(String[] args) {
 		int[] cells = {1, 0, 0, 0, 0, 1, 0, 0};
-		int days = 2;
+		int days = 1;
 		cells = DayChange.change(cells, days);
 		for (int n : cells) {
 			System.out.printf("%d ", n);
