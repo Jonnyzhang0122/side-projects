0a6219c4be30db0afd8782a4daf117c402df1c95
MrHohn <zzhonzi@gmail.com>
2016-03-02 17:44:07 -0500

    Reconstruct prepare folder

---
+++ prepare/Google/OA/DuplicateOne.java
@@ -0,0 +1,36 @@
+/* 
+input range [1, 10,000,000]
+1234 -> 11234, 12234, 12334, 12344 -> return 12344
+1000 -> 11000, 10000 -> return 11000
+9378 -> 99378, 93378, 93778, 93788 -> return 99378
+
+methodology: loop from the left, once current number is larger than the next one (or is the last number), duplicate it
+*/
+public class DuplicateOne {
+	public static int duplicateOne(int num) {
+		StringBuilder res = new StringBuilder();
+		String numS = "" + num;
+		boolean duplicated = false;
+		for (int i = 0; i < numS.length() - 1; ++i) {
+			res.append(numS.charAt(i));
+			if (!duplicated && numS.charAt(i) - numS.charAt(i + 1) > 0) {
+				res.append(numS.charAt(i));
+				duplicated = true;
+			}
+		}
+		res.append(numS.charAt(numS.length() - 1));
+		if (!duplicated) {
+			res.append(numS.charAt(numS.length() - 1));
+		}
+		return Integer.parseInt(res.toString());
+	}
+
+	public static void main(String[] args) {
+		int test1 = 1234;
+		int test2 = 1000;
+		int test3 = 9378;
+		System.out.println(test1 + " -> " + DuplicateOne.duplicateOne(test1));
+		System.out.println(test2 + " -> " + DuplicateOne.duplicateOne(test2));
+		System.out.println(test3 + " -> " + DuplicateOne.duplicateOne(test3));
+	}
+}
