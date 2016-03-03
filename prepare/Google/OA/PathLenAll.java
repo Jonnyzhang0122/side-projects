73bd25c75e86df386890a9ce089f0b8738913220
MrHohn <zzhonzi@gmail.com>
2016-03-03 13:23:25 -0500

    Added Path Len All

---
+++ prepare/Google/OA/PathLenAll.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+public class PathLenAll {
+	public static int sum(String input) {
+		String[] paths = input.trim().split("\n");
+		
+		Stack<String> stack = new Stack<>();
+		HashSet<Integer> foundLevels = new HashSet<>();
+		int sum = 0, curLen = 0;
+		for (String curPath : paths) {
+			// count the spaces ahead
+			int space = 0;
+			for (int i = 0; i < curPath.length(); ++i) {
+				if (curPath.charAt(i) == ' ') {
+					++space;
+				}
+				else {
+					break;
+				}
+			}
+
+			// for same level or level up (do not need to pop if level down)
+			// pop out all content including the same level
+			while (space < stack.size()) {
+				// reset found flag if level up
+				if (space + 1 < stack.size()) {
+					// check whether need to remove
+					if (foundLevels.contains(space + 1)) {
+						foundLevels.remove(space + 1);
+					}
+				}
+				curLen -= (stack.pop().length() + 1);
+			}
+
+			// check if it is image
+			if (!foundLevels.contains(space) && (curPath.endsWith(".jpeg") || curPath.endsWith(".gif") || curPath.endsWith(".png"))) {
+				sum += curLen + 1; // +1 for slash
+				foundLevels.add(space);
+
+				for (String file : stack) {
+					System.out.printf("/%s", file);
+				}
+				System.out.printf("/\n");
+			}
+
+			// append current path len
+			curLen += curPath.length() - space + 1;
+			stack.push(curPath.trim());
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		String test1 = "dir\nddir\n a.txt\n b.jpeg\n c.gif\nddir2\ndddir\nddddir\n aaa.exe\n";
+		System.out.println("Input:\n" + test1);
+		System.out.println(PathLenAll.sum(test1));
+		System.out.println("");
+
+		String test2 = "dir\nddir\n a.txt\n b.jpeg\n c.gif\nddir2\ndddir\nddddir\n aaa.exe\ndir1\n dir2\n  dir3\n   what.png";
+		System.out.println("Input:\n" + test2);
+		System.out.println("Output:\n" + PathLenAll.sum(test2));
+		System.out.println("");
+
+		String test3 = "dir\nddir\n a.txt\n b.jpeg\n c.gif\nddir2\ndddir\nddddir\n aaa.exe\ndir1\n dir2\n  dir3\n   what.png\n   dir4\n    new.jpeg";
+		System.out.println("Input:\n" + test3);
+		System.out.println("Output:\n" + PathLenAll.sum(test3));
+		System.out.println("");
+
+		String test4 = "dir\nddir\n a.txt\n b.jpeg\n c.gif\nddir2\ndddir\nddddir\n aaa.exe\ndir1\n dir2\n  dir3\n   what.png\n   dir4\n    new.jpeg\n   new1.gif";
+		System.out.println("Input:\n" + test4);
+		System.out.println("Output:\n" + PathLenAll.sum(test4));
+		System.out.println("");
+	}
+}
