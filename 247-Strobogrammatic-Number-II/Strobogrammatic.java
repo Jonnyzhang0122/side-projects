07c59297e71005da62dc68218f14ce4e61694c7a
MrHohn <zzhonzi@gmail.com>
2016-03-18 11:06:11 -0400

    Rewrote Strobogrammatic for neater version

--- 247-Strobogrammatic-Number-II/Strobogrammatic.java
+++ 247-Strobogrammatic-Number-II/Strobogrammatic.java
@@ -1,3 +1,25 @@
+// neat version
+public class Solution {
+    public List<String> findStrobogrammatic(int n) {
+        return helper(n, n);
+    }
+    
+    private List<String> helper(int cur, int n) {
+        if (cur == 0) return new ArrayList<String>(Arrays.asList(""));
+        if (cur == 1) return new ArrayList<String>(Arrays.asList("0", "1", "8"));
+        
+        List<String> mids = helper(cur - 2, n);
+        List<String> res = new ArrayList<>();
+        for (String mid : mids) {
+            if (cur != n) res.add("0" + mid + "0");
+            res.add("1" + mid + "1");
+            res.add("8" + mid + "8");
+            res.add("6" + mid + "9");
+            res.add("9" + mid + "6");
+        }
+        return res;
+    }
+}
 
 
 // first accepcted version
