ac5e0a858fcc95ce65eccddec293c2770d7f7108
MrHohn <zzhonzi@gmail.com>
2016-03-26 12:16:31 -0400

    Rewrote Longest Increasing Subsequence, self implemented binary search

--- 300-Longest-Increasing-Subsequence/LongestLIS.java
+++ 300-Longest-Increasing-Subsequence/LongestLIS.java
@@ -1,3 +1,49 @@
+// more readable version, self implemented binary search
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) return 0;
+        List<Integer> seq = new ArrayList<>();
+        for (int n : nums) {
+            int pos = getPos(seq, n);
+            // if new number is smaller than the first number in list
+            if (pos == -1) continue;
+            // if seq is empty or current number is larger than the largest number in seq
+            else if (pos == seq.size()) {
+                seq.add(n);
+            }
+            // find correct position an replace previous value
+            else {
+                seq.set(pos, n);
+            }
+        }
+        return seq.size();
+    }
+    
+    private int getPos(List<Integer> seq, int newVal) {
+        if (seq.size() == 0) return 0;
+        if (newVal > seq.get(seq.size() - 1)) return seq.size();
+        
+        // do binary search
+        int start = 0, end = seq.size() - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (seq.get(mid) == newVal) {
+                return mid;
+            }
+            else if (seq.get(mid) > newVal) {
+                end = mid;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        
+        if (seq.get(start) > newVal) return start;
+        return end;
+    }
+}
+
+
 // binary search version
 public class Solution {
     public int lengthOfLIS(int[] nums) {
@@ -22,7 +68,7 @@ public class Solution {
 }
 
 
-// N^2 version
+// O(n^2) DP version
 public class Solution {
     public int lengthOfLIS(int[] nums) {
         if (nums.length <= 1) {
