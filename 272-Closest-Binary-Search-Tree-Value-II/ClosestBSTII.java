ce56842691ba02622f1541126a23e7dcd9c2d9b5
MrHohn <zzhonzi@gmail.com>
2016-03-26 22:37:42 -0400

    Rewrote Closest BST, solution with stacks

--- 272-Closest-Binary-Search-Tree-Value-II/ClosestBSTII.java
+++ 272-Closest-Binary-Search-Tree-Value-II/ClosestBSTII.java
@@ -1,4 +1,87 @@
-// improved O(n) solution
+// O(klgN) solution with stacks
+public class Solution {
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> succ = new Stack<>();
+        Stack<TreeNode> pred = new Stack<>();
+        initSucc(root, target, succ);
+        initPred(root, target, pred);
+        if (!succ.isEmpty() && !pred.isEmpty() && succ.peek().val == pred.peek().val) getPred(pred);
+        
+        int num = 0;
+        while (num < k) {
+            ++num;
+            if (succ.isEmpty()) res.add(getPred(pred));
+            else if (pred.isEmpty()) res.add(getSucc(succ));
+            else {
+                double succDiff = Math.abs((double)succ.peek().val - target);
+                double predDiff = Math.abs((double)pred.peek().val - target);
+                if (succDiff < predDiff) {
+                    res.add(getSucc(succ));
+                }
+                else {
+                    res.add(getPred(pred));
+                }
+            }
+        }
+        return res;
+    }
+    
+    private void initSucc(TreeNode root, double target, Stack<TreeNode> succ) {
+        while (root != null) {
+            if ((double)root.val == target) {
+                succ.push(root);
+                return;
+            }
+            else if ((double)root.val > target) {
+                succ.push(root);
+                root = root.left;
+            }
+            else {
+                root = root.right;
+            }
+        }
+    }
+    
+    private void initPred(TreeNode root, double target, Stack<TreeNode> pred) {
+        while (root != null) {
+            if ((double)root.val == target) {
+                pred.push(root);
+                return;
+            }
+            else if ((double)root.val < target) {
+                pred.push(root);
+                root = root.right;
+            }
+            else {
+                root = root.left;
+            }
+        }
+    }
+    
+    private int getSucc(Stack<TreeNode> succ) {
+        int ret = succ.peek().val;
+        TreeNode next = succ.pop().right;
+        while (next != null) {
+            succ.push(next);
+            next = next.left;
+        }
+        return ret;
+    }
+    
+    private int getPred(Stack<TreeNode> pred) {
+        int ret = pred.peek().val;
+        TreeNode next = pred.pop().left;
+        while (next != null) {
+            pred.push(next);
+            next = next.right;
+        }
+        return ret;
+    }
+}
+
+
+// improved O(n) solution with queue
 public class Solution {
     public List<Integer> closestKValues(TreeNode root, double target, int k) {
         Queue<Integer> queue = new LinkedList<>();
@@ -25,7 +108,7 @@ public class Solution {
 }
 
 
-// inorder travelsal solution
+// inorder travelsal solution with heap
 public class Solution {
     private class Candidate {
         int val;
