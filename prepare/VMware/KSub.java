6b2b436c0259d156c360e76d84b4c77679da75fc
MrHohn <zzhonzi@gmail.com>
2016-04-01 10:47:34 -0400

    Added KSub

---
+++ prepare/VMware/KSub.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class KSub {
+	public static int kSub(int[] nums, int k) {
+		if (k == 0 || nums.length == 0) {
+			return -1;
+		}
+
+		int[] prefix = new int[nums.length + 1];
+		for (int i = 1; i <= nums.length; ++i) {
+			prefix[i] = prefix[i - 1] + nums[i - 1];
+		}
+
+		int count = 0;
+		// outer loop changes the size of window
+		for (int i = 1; i <= nums.length; ++i) {
+			// inner loop changes the starting point
+			for (int j = i; j <= nums.length; ++j) {
+				int curSum = prefix[j] - prefix[j - i];
+				if (curSum % k == 0) ++count;
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		int[] test1 = {1, 2, 3, 4, 1};
+		int k = 3;
+		System.out.println(KSub.kSub(test1, k));
+		System.out.println("");
+
+		int[] test2 = {1, 2, 3, 4, 5};
+		System.out.println(KSub.kSub(test2, k));
+		System.out.println("");
+
+		int[] test3 = {1, 4, 0, 7};
+		System.out.println(KSub.kSub(test3, k));
+		System.out.println("");
+	}
+}
