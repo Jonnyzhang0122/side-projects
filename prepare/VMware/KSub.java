4ed6491d59fc88931bb60b3dcec9c215971f858a
MrHohn <zzhonzi@gmail.com>
2016-04-03 10:06:49 -0400

    Added O(N+K) solution for KSub

--- prepare/VMware/KSub.java
+++ prepare/VMware/KSub.java
@@ -6,22 +6,46 @@ public class KSub {
 			return -1;
 		}
 
-		int[] prefix = new int[nums.length + 1];
-		for (int i = 1; i <= nums.length; ++i) {
-			prefix[i] = prefix[i - 1] + nums[i - 1];
+		int[] modCount = new int[k];
+		modCount[0] = 1;
+		int sum = 0;
+		for (int n : nums) {
+			sum += n;
+			sum %= k;
+			// count # of prefix sum with remainder 0 to k - 1 when divide k
+			++modCount[sum];
 		}
 
-		int count = 0;
-		// outer loop changes the size of window
-		for (int i = 1; i <= nums.length; ++i) {
-			// inner loop changes the starting point
-			for (int j = i; j <= nums.length; ++j) {
-				int curSum = prefix[j] - prefix[j - i];
-				if (curSum % k == 0) ++count;
-			}
+		int res = 0;
+		for (int count : modCount) {
+			// count of all pairs from prefix sum that have the same remainder
+			res += count * (count - 1) / 2;
 		}
-		return count;
+		return res;
 	}
+
+	// slow solution
+	// public static int kSub(int[] nums, int k) {
+	// 	if (k == 0 || nums.length == 0) {
+	// 		return -1;
+	// 	}
+
+	// 	int[] prefix = new int[nums.length + 1];
+	// 	for (int i = 1; i <= nums.length; ++i) {
+	// 		prefix[i] = prefix[i - 1] + nums[i - 1];
+	// 	}
+
+	// 	int count = 0;
+	// 	// outer loop changes the size of window
+	// 	for (int i = 1; i <= nums.length; ++i) {
+	// 		// inner loop changes the starting point
+	// 		for (int j = i; j <= nums.length; ++j) {
+	// 			int curSum = prefix[j] - prefix[j - i];
+	// 			if (curSum % k == 0) ++count;
+	// 		}
+	// 	}
+	// 	return count;
+	// }
 
 	public static void main(String[] args) {
 		int[] test1 = {1, 2, 3, 4, 1};
